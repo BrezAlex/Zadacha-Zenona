c4272f409236366ccff7a16a0683bb552447c14e
Alexandr Brezin <sasha.brezin7262@gmail.com>
2020-04-22 17:58:53 +0300

    Ничего

---
+++ ZadachaZenona/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ZadachaZenona
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 129);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 309);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт реальность";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "5";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "1";
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Скорость Ахилеса";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Скорость черепахи";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(507, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 26);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Старт теория";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(507, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 36);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Стоп";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button3_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ZadachaZenona/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ZadachaZenona
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double vAxiles, vCherapasha;
+        double x, y;
+        double time, dTime;
+        double difference;
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics grf = e.Graphics;
+            Pen Pen = new Pen(Color.Black, 3);
+            Pen Pen_2 = new Pen(Color.Gray, 3);
+            grf.DrawEllipse(Pen, (float)x, pictureBox1.Height-100, 100, 100);
+            grf.DrawEllipse(Pen_2, (float)y, pictureBox1.Height - 50, 50, 50);           
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (x > Width)
+            {
+                x = -100;
+            } 
+            if (y>Width)
+            {
+                y = -100;
+            }            
+            x += vAxiles;
+            y += vCherapasha;
+            pictureBox1.Refresh();
+        }
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+           
+            double.TryParse(textBox1.Text, out vAxiles);
+            double.TryParse(textBox2.Text, out vCherapasha);           
+            timer1.Start();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) == true) return;
+            if (e.KeyChar == Convert.ToChar(Keys.Back)) return;
+            e.Handled = true;            
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) == true) return;
+            if (e.KeyChar == Convert.ToChar(Keys.Back)) return;
+            e.Handled = true;            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (x > Width)
+            {
+                x = -100;
+            }
+            if (y > Width)
+            {
+                y = -100;
+            }
+            difference = y - x;
+            time = difference / vAxiles;
+            dTime = time / 1000;
+            x += vAxiles * dTime;
+            y += vCherapasha * dTime;
+            pictureBox1.Refresh();
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            double.TryParse(textBox1.Text, out vAxiles);
+            double.TryParse(textBox2.Text, out vCherapasha);
+            timer2.Start();
+        }
+        private void button3_MouseClick(object sender, MouseEventArgs e)
+        {            
+            timer1.Stop();
+            timer2.Stop();
+        } 
+        
+    }
+}
