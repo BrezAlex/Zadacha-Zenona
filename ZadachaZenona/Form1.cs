3ca2979d0617652186b194566a8a089784f69da3
Alexandr Brezin <sasha.brezin7262@gmail.com>
2020-05-07 14:11:32 +0300

    Чуть чуть

--- ZadachaZenona/Form1.cs
+++ ZadachaZenona/Form1.cs
@@ -10,29 +10,24 @@ namespace ZadachaZenona
         {
             InitializeComponent();
         }
-        double _speedAxiles;
-        double _speedCherapasha;
-        double _x;
-        double _y;
+
+        private double _speedAxiles;  //
+        private double _speedCherapasha;
+        private double _x;
+        private double _y;
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
             Graphics grf = e.Graphics;
-            Pen Pen = new Pen(Color.Black, 3);
-            Pen Pen_2 = new Pen(Color.Gray, 3);
-            grf.DrawEllipse(Pen, (float)_x, pictureBox1.Height-100, 100, 100);
-            grf.DrawEllipse(Pen_2, (float)_y, pictureBox1.Height - 50, 50, 50);           
+            Pen pen = new Pen(Color.Black, 3);
+            Pen pen2 = new Pen(Color.Gray, 3);
+            grf.DrawEllipse(pen, (float)_x+100, pictureBox1.Height-100, 100, 100);
+            grf.DrawEllipse(pen2, (float)_y, pictureBox1.Height - 50, 50, 50);           
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (_x > Width)
-            {
-                _x = -100;
-            } 
-            if (_y > Width)
-            {
-                _y = -100;
-            }            
+            if (_x > Width) _x = -100;
+            if (_y > Width) _y = -100;
             _x += _speedAxiles;
             _y += _speedCherapasha;
             pictureBox1.Refresh();
@@ -47,31 +42,25 @@ namespace ZadachaZenona
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (Char.IsDigit(e.KeyChar) == true) return;
+            if (char.IsDigit(e.KeyChar)) return;
             if (e.KeyChar == Convert.ToChar(Keys.Back)) return;
             e.Handled = true;            
         }
 
         private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (Char.IsDigit(e.KeyChar) == true) return;
+            if (char.IsDigit(e.KeyChar)) return;
             if (e.KeyChar == Convert.ToChar(Keys.Back)) return;
             e.Handled = true;            
         }
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-            if (_x > Width)
-            {
-                _x = -100;
-            }
-            if (_y > Width)
-            {
-                _y = -100;
-            }
+            if (_x > Width) _x = -100;
+            if (_y > Width) _y = -100;
             double difference = _y - _x;
             double time = difference / _speedAxiles;
-            double dTime = time / 1000;
+            double dTime = time / 150;
             _x += _speedAxiles * dTime;
             _y += _speedCherapasha * dTime;
             pictureBox1.Refresh();
